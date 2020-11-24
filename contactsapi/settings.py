521a53891785d6b3b8bc026990472935ff1a21fd
opeeny <godfreyopeeny@gmail.com>
2020-11-24 09:29:14 +0300

    set jwt authentication

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'authentication_app'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication_app.backendjwt.JWTAuthentication',
+        )
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -114,6 +119,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+#JWT
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
 
 # Static files (CSS, JavaScript, Images)
